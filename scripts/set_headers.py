dbbbb844f80e73f5743bd2d213e21e44937c1870
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2017-03-19 21:58:23 +0000

    Add unauthenticated message if sign-in incorrect

--- scripts/set_headers.py
+++ scripts/set_headers.py
@@ -1,14 +1,23 @@
 import requests
+from mitmproxy import http
 from mitmproxy import ctx
 
 # TODO authenticate user
-def getUser(flow):
-        if 'proxyauth' in flow.metadata: # TODO and user != None
-            return flow.metadata['proxyauth'][0]
-        else:
-            return ""
+def getProxyauth(flow):
+    if 'proxyauth' in flow.metadata: # TODO and user != None
+        return flow.metadata['proxyauth']
+    else:
+        return ""
 
 def request(flow):
-    payload = { 'username': getUser(flow), 'url': flow.request.pretty_host }
+    payload = { 'username': getProxyauth(flow)[0], 'password': getProxyauth(flow)[1], 'url': flow.request.pretty_host }
     r = requests.get("http://localhost:3000/idea", params=payload)
+    # TODO check if auth'd
+    if r.status_code == 401:
+        flow.response = http.HTTPResponse.make(
+            401,  # (optional) status code
+            b"You need to authenticate properly, buddy",  # (optional) content
+            {"Content-Type": "text/html"}  # (optional) headers
+        )
+        return
     flow.request.headers["x-idea-id"] = r.text
